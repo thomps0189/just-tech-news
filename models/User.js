6627a6c4ec037dc72f2fa7789ce6357527ec8f8a
thomps0189 <angelathompson2011@gmail.com>
2022-01-05 14:37:11 -0600

    post model

--- models/User.js
+++ models/User.js
@@ -10,70 +10,52 @@ class User extends Model {
   }
 }
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
-  {// define an id column
-      id: {
-          // use the speciali Sequelize DataTypes object provide what type of data it is
-          type: DataTypes.INTEGER,
-          //this is the equivalent of SQL's "NOT NULL" option
-          allowNull: false,
-          //instruct that this is the Primary Key
-          primaryKey: true,
-          // turn on auto increment
-          autoIncrement: true
-      },
-      // define a username column 
-      username: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      // define an email column 
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // there cannot be any duplicate email values in this table
-        unique: true, 
-        // if allowNull is set to false, we can run our data through validators before creating the table data
-        validate: {
-            isEmail: true
-        }
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4]
-        }
-      }
-    // TABLE COLUMN DEFINITIONS GO HERE
-  },
   {
-    hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-        async beforeCreate(newUserData) {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
-        },
-        // set up beforeUpdate lifecycle "hook" functionality
-        async beforeUpdate(updatedUserData) {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
-        }
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-    // pass in our imported sequelize connection (the direct connection to our database)
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: 'user'
   }
 );
