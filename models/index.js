6df55591891a9d7da115aa22e81e4bb40817f858
thomps0189 <angelathompson2011@gmail.com>
2022-01-11 10:41:08 -0600

    comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Vote = require('./Vote');
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -38,6 +39,22 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
   
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-
+const commentRoutes = require('./comment-routes');
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
